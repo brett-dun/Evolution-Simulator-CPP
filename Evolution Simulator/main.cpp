90d0a2f671493e12b3ca9f59c5c5d3bb2db42ceb
Brett Duncan <brettd314@gmail.com>
2016-06-28 21:58:01 -0500

    Initial Commit

---
+++ Evolution Simulator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Evolution Simulator
+//
+//  Created by Brett Duncan on 6/28/16.
+//  Copyright © 2016 Brett James Duncan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
